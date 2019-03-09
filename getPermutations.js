b2545586c785f11e994cd7f375e650fba863e8a1
Chris Athanas <realityexpanderdev@gmail.com>
2019-03-09 16:56:39 -0600

    Create getPermutations.js

---
+++ getPermutations.js
@@ -0,0 +1,17 @@
+function getPermutations(string) {
+
+  // Base case
+  if (string.length <= 1) {
+    return new Set([string]);
+  }
+
+  const allCharsExceptLast = string.slice(0, -1);
+  const lastChar = string[string.length - 1];
+
+  // Recursive call: get all possible permutations for all chars except last
+  const permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
+
+
+
+  return permutations;
+}
